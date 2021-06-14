3feef16acbb407248cad01dcdbc5c35d62f924b8
Tom Dirinck <tom.dirinck@gmail.com>
2021-06-14 16:28:56 +0200

    Added customer model.

--- Model/Customer.php
+++ Model/Customer.php
@@ -3,13 +3,17 @@ declare(strict_types=1);
 
 class Customer
 {
+    private int $id;
+    private int $groupId;
     private string $firstname;
     private string $lastname;
     private int $fixedDiscount;
     private int $variableDiscount;
 
-    public function __construct(string $firstname, string $lastname, int $fixedDiscount, int $variableDiscount)
+    public function __construct(string $firstname, string $lastname, int $fixedDiscount, int $variableDiscount, int $id, int $groupId)
     {
+        $this->id = $id;
+        $this->groupId = $groupId;
         $this->firstname = $firstname;
         $this->lastname = $lastname;
         $this->fixedDiscount = $fixedDiscount;
@@ -38,5 +42,21 @@ class Customer
     public function getVariableDiscount(): int
     {
         return $this->variableDiscount;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of groupId
+     */
+    public function getGroupId(): int
+    {
+        return $this->groupId;
     }
 }
