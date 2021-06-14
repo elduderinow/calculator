48be31db18d0f07608caefec287eb44734d8dddb
Tom Dirinck <tom.dirinck@gmail.com>
2021-06-14 17:13:03 +0200

    Placed temp pdo query connection.

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -6,9 +6,15 @@ class HomepageController
     //render function with both $_GET and $_POST vars available if it would be needed.
     public function render(array $GET, array $POST)
     {
-        //this is just example code, you can remove the line below
-        //you should not echo anything inside your controller - only assign vars here
-        // then the view will actually display them.
+        $pdo = Connection::Open();
+
+        $handle = $pdo->prepare('SELECT product.name, product.price FROM product ORDER BY product.name');
+        $handle->execute();
+        $products = $handle->fetchAll();
+
+        $handle = $pdo->prepare('SELECT customer.firstname, customer.lastname, customer.id FROM customer ORDER BY customer.firstname');
+        $handle->execute();
+        $customers = $handle->fetchAll();
 
         //load the view
         require 'View/homepage.php';
