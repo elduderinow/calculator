2768c7842f2d2f1124be99dc81726fb0f300eb89
elduderinow <yarrut.franken@gmail.com>
2021-06-14 16:37:25 +0200

    solving conflict

--- Model/pdo_SQL.php
+++ Model/pdo_SQL.php
@@ -17,3 +17,12 @@ function openConnection(): PDO
 }
 
 $pdo = openConnection();
+
+$handle = $pdo->prepare('SELECT product.name, product.price FROM product ORDER BY product.name');
+$handle->execute();
+$products = $handle->fetchAll();
+
+$handle = $pdo->prepare('SELECT customer.firstname, customer.lastname, customer.id FROM customer ORDER BY customer.firstname');
+$handle->execute();
+$customers = $handle->fetchAll();
+
--- index.php
+++ index.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 //include all your model files here
 require 'Model/pdo_SQL.php';
-require 'Controller/get_calctable.php';
 require 'Model/Customer.php';
+
+
 //include all your controllers here
+require 'Controller/HomepageController.php';
+$controller = new HomepageController();
+$controller->render($_GET, $_POST);
 
-
-include 'view/homepage.php';
