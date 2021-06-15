3163341dc4409fa52df168fce5b7d8741c5cffb6
Tom Dirinck <tom.dirinck@gmail.com>
2021-06-15 16:13:52 +0200

    Cleanup. Modified price getter.

--- Model/Product.php
+++ Model/Product.php
@@ -1,31 +1,26 @@
 <?php
 declare(strict_types=1);
 
-class Product
-{
+class Product {
     private int $id;
     private string $name;
     private int $price;
 
-    public function __construct(int $id, string $name, int $price)
-    {
+    public function __construct(int $id, string $name, int $price) {
         $this->id = $id;
         $this->name = $name;
         $this->price = $price;
     }
 
-    public function getId(): int
-    {
+    public function getId(): int {
         return $this->id;
     }
 
-    public function getName(): string
-    {
+    public function getName(): string {
         return $this->name;
     }
 
-    public function getPrice(): int
-    {
-        return $this->price;
+    public function getPrice(): float {
+        return $this->price / 100;
     }
 }
