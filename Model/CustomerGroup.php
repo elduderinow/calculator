3a69dfa2f84bd81bd15a12d3dcf70ee3f383dd24
JonasRossou123 <jonas.rossou@hotmail.com>
2021-06-15 08:59:12 +0200

    added 2 classes

---
+++ Model/CustomerGroup.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+class CustomerGroup
+{
+    private int $id;
+    private string $name;
+    private int $parentId;
+    private int $fixedDiscount;
+    private int $variableDiscount;
+
+    public function __construct(int $id, string $name, int $parentId, int $fixedDiscount, int $variableDiscount)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->parentId = $parentId;
+        $this->fixedDiscount = $fixedDiscount;
+        $this->variableDiscount = $variableDiscount;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getParentId(): int
+    {
+        return $this->parentId;
+    }
+
+    public function getFixedDiscount(): int
+    {
+        return $this->fixedDiscount;
+    }
+
+        public function getVariableDiscount(): int
+    {
+        return $this->variableDiscount;
+    }
+}
