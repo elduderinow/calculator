1bc5f4464e392ce9b9d012d7fe6c1cc7891ea1e7
elduderinow <yarrut.franken@gmail.com>
2021-06-17 09:58:29 +0200

    fixed errors on homepage

--- View/homepage.php
+++ View/homepage.php
@@ -2,83 +2,89 @@
 
 
 <body>
-  <div class="container">
+<div class="container">
     <div class="row">
-      <div class="col-sm products">
-        <!-- select customer checkout -->
-        <div class="row">
-          <div class="col-6">
-            <form method="post">
-              <label for="customer-id">Choose a customer:</label>
-              <select name="customer-id" id="customer-id">
-                <?php foreach ($customers as $customer): ?>
-                <option <?php
-                echo(isset($_SESSION['customer-id']) == $customer->getId() ? 'selected' : '');
-                ?> value=<?php echo '{"id":' . $customer->getId() . ',"groupId":' . $customer->getGroupId() . '}'?>>
-                  <?php echo htmlspecialchars($customer->getFullName()) ?>
-                </option>
+        <div class="col-sm products">
+            <!-- select customer checkout -->
+            <div class="row">
+                <div class="col-6">
+                    <form method="post">
+                        <label for="customer-id">Choose a customer:</label>
+                        <select name="customer-id" id="customer-id">
+                            <?php foreach ($customers as $customer): ?>
+                                <option <?php
+                                if (isset($_SESSION['customer-id'])) {
+                                    echo($_SESSION['customer-id'] == $customer->getId() ? "selected" : "");
+                                }
+                                ?> value= <?php echo '{"id":' . $customer->getId() . ',"groupId":' . $customer->getGroupId() . '}' ?>>
+                                    <?php echo htmlspecialchars($customer->getFullName()) ?>
+                                </option>
+                            <?php endforeach; ?>
+                            <input type="submit" value="Submit">
+                        </select>
+                    </form>
+                </div>
+            </div>
+
+            <!-- products -->
+            <table class="products table table-striped table-wide ">
+                <thead>
+                <tr>
+                    <th width="40%">Product</th>
+                    <th width="40%">Price</th>
+                    <td colspan="2" width="20%"></td>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($products as $product): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($product->getName()) ?></td>
+                        <td><?php echo '€' . htmlspecialchars($product->getPrice()) ?></td>
+                        <td>
+                            <form method="get">
+                                <input type="hidden" name="id" value="<?php echo $product->getId() ?>"/>
+                                <input type="submit" name="button" value="Add" class="btn btn-success">
+                            </form>
+                        </td>
+                        <td>
+                            <form method="get">
+                                <input type="hidden" name="id" value="<?php echo $product->getId() ?>"/>
+                                <input type="submit" name="button" value="Delete" class="btn btn-danger">
+                            </form>
+                        </td>
+                    </tr>
                 <?php endforeach; ?>
-                <input type="submit" value="Submit">
-              </select>
-            </form>
-          </div>
+                </tbody>
+            </table>
+
+            <!-- total checkout -->
+            <table class="total table table-striped table-wide">
+                <thead>
+                <tr>
+                    <th width="40%">Added products</th>
+                    <th width="40%">Total Price</th>
+                    <td colspan="2" width="20%"></td>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Mouse pad</td>
+                    <td>54.18€</td>
+                    <td>
+                        <form class="checkout" method="post">
+                            <input type="hidden" name="id" value=""/>
+                            <input type="submit" name="delete" value="Checkout" class="btn btn-primary">
+                        </form>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+
         </div>
-
-        <!-- products -->
-        <table class="products table table-striped table-wide ">
-          <thead>
-            <tr>
-              <th width="40%">Product</th>
-              <th width="40%">Price</th>
-              <td colspan="2" width="20%"></td>
-            </tr>
-          </thead>
-          <tbody>
-            <?php foreach ($products as $product): ?>
-            <tr>
-              <td><?php echo htmlspecialchars($product->getName()) ?></td>
-              <td><?php echo '€' . htmlspecialchars($product->getPrice()) ?></td>
-              <td>
-                <a href="" class="btn btn-success">Add</a>
-              </td>
-              <td>
-                <form method="post">
-                  <input type="hidden" name="id" value="" />
-                  <input type="submit" name="delete" value="Delete" class="btn btn-danger">
-                </form>
-              </td>
-            </tr>
-            <?php endforeach; ?>
-          </tbody>
-        </table>
-
-        <!-- total checkout -->
-        <table class="total table table-striped table-wide">
-          <thead>
-            <tr>
-              <th width="40%">Added products</th>
-              <th width="40%">Total Price</th>
-              <td colspan="2" width="20%"></td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Mouse pad</td>
-              <td>54.18€</td>
-              <td>
-                <form class="checkout" method="post">
-                  <input type="hidden" name="id" value="" />
-                  <input type="submit" name="delete" value="Checkout" class="btn btn-primary">
-                </form>
-              </td>
-            </tr>
-
-          </tbody>
-        </table>
-
-      </div>
     </div>
 </body>
 <?php
 require 'includes/footer.php'
 ?>
+
